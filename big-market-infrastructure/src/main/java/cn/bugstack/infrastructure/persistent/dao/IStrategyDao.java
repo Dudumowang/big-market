1d4e14ee616ebcef8f5000e09f750b63a621e2ec
Dudumowang <111416459+Dudumowang@users.noreply.github.com>
2025-03-29 01:57:57 +0800

    概率装配完成测试

--- big-market-infrastructure/src/main/java/cn/bugstack/infrastructure/persistent/dao/IStrategyDao.java
+++ big-market-infrastructure/src/main/java/cn/bugstack/infrastructure/persistent/dao/IStrategyDao.java
@@ -3,8 +3,6 @@ package cn.bugstack.infrastructure.persistent.dao;
 import cn.bugstack.infrastructure.persistent.po.Strategy;
 import org.apache.ibatis.annotations.Mapper;
 
-import java.util.List;
-
 /**
  * @Author: 杨文彬
  * @Description: 抽奖策略DAO
--- big-market-infrastructure/src/main/java/cn/bugstack/infrastructure/persistent/dao/IStrategyRuleDao.java
+++ big-market-infrastructure/src/main/java/cn/bugstack/infrastructure/persistent/dao/IStrategyRuleDao.java
@@ -3,7 +3,6 @@ package cn.bugstack.infrastructure.persistent.dao;
 import cn.bugstack.infrastructure.persistent.po.StrategyRule;
 import org.apache.ibatis.annotations.Mapper;
 
-import java.util.List;
 
 /**
  * @Author: 杨文彬
